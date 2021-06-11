caa4da96822978d5ebcf9b318db98129a3b72241
mhutzkie <donnamadronio09@gmail.com>
2021-06-11 01:16:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-annk2a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-annk2a)
---
+++ src/app/bago/bago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bago',
+  templateUrl: './bago.component.html',
+  styleUrls: ['./bago.component.css']
+})
+export class BagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
